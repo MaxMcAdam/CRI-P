3d1b476ec8709c3d7f3a7a2fb2619235953bc3f5
MaxMcAdam <max@fredcom.com>
2019-10-24 07:59:56 -0700

    Add env var support

--- imageservice/client/main.go
+++ imageservice/client/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	cri "github.com/kubernetes/kubernetes/staging/src/k8s.io/cri-api/pkg/apis/runtime/v1alpha2"
 	"google.golang.org/grpc"
+	"os"
 	"time"
 )
 
@@ -14,6 +15,10 @@ const (
 )
 
 func main() {
+	image := IMAGE
+	if len(os.Args) > 1 {
+		image = os.Args[1]
+	}
 	// Create grpc connection
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
@@ -24,11 +29,11 @@ func main() {
 
 	// Create cri image service client
 	ic := cri.NewImageServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	// Pull the specified image
-	resp, err := ic.PullImage(ctx, &cri.PullImageRequest{Image: &cri.ImageSpec{Image: IMAGE}})
+	resp, err := ic.PullImage(ctx, &cri.PullImageRequest{Image: &cri.ImageSpec{Image: image}})
 	if err != nil {
 		fmt.Printf("Error making pull image request: %v\n", err)
 		return
